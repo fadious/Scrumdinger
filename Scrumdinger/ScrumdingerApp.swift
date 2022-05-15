cf2baa16d53fd6073a7bde6e50370acb50a400d1
Fadi Sayfi <fsayfi@icloud.com>
2022-05-15 09:31:54 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Fadi Sayfi on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
